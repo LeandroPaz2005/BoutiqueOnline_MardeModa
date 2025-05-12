68b3687c20674482b9b350c45cae06463b2e2401
LeandroPazK <U223199603@utp.edu.pe>
2025-05-12 11:39:02 -0500

    Primer commit: proyecto base

---
+++ src/main/java/BoutiqueOnline/BoutiqueOnlineApplication.java
@@ -0,0 +1,13 @@
+package BoutiqueOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiqueOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutiqueOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BoutiqueOnline/BoutiqueOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package BoutiqueOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoutiqueOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
